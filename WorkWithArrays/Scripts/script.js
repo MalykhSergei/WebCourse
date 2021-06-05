1bae8ce360af5d38a9101136c323f3ce9e506381
Sergei Malykh <sergej.malykh@gmail.com>
2021-06-05 21:30:31 +0200

    WorkWithArrays

---
+++ WorkWithArrays/Scripts/script.js
@@ -0,0 +1,61 @@
+﻿(function () {
+
+    "use strict";
+
+    var array = [3, 2, 1, 5, 8, -2, 0, 6, 12, 9, 5, 7, 1, -4, -1, -8];
+
+    function sortByDescending(array) {
+        var sortedArray = array.sort(function (e1, e2) {
+            return e2 - e1;
+        });
+
+        return sortedArray;
+    }
+
+    console.log("Отсортированный массив по убыванию: " + sortByDescending(array));
+
+    function getFirstArrayItems(array) {
+        return array.slice(0, 5);
+    }
+
+    console.log("Подмассив из первых 5 элементов: " + getFirstArrayItems(array));
+
+    function getLastArrayItems(array) {
+        return array.slice(array.length - 5);
+    }
+
+    console.log("Подмассив из последних 5 элементов: " + getLastArrayItems(array));
+
+    function getEvenNumbersSum(array2) {
+        return array2.filter(function (item) {
+            return item % 2 === 0;
+        }).reduce(function (sum, currentItem) {
+            return sum + currentItem;
+        },
+            0);
+    }
+
+    console.log("Сумма элементов массива, которые являются четными числами: " + getEvenNumbersSum(array));
+
+    function getArray(startIndex, lastIndex) {
+        var array = [];
+
+        for (var i = startIndex; i <= lastIndex; i++) {
+            array.push(i);
+        }
+
+        return array;
+    }
+
+    console.log("Массив чисел от 1 до 100: " + getArray(1, 100));
+
+    function getSquares(array) {
+        return array.filter(function (evenNumbers) {
+            return evenNumbers % 2 === 0;
+        }).map(function (currentValue) {
+            return currentValue * currentValue;
+        });
+    }
+
+    console.log("Список квадратов четных чисел: " + getSquares(getArray(1, 100)));
+})()
