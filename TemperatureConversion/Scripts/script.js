1e5d3d38d52a0a4bafe058ef1d331e05f7bcf901
Sergei Malykh <sergej.malykh@gmail.com>
2021-06-09 00:07:54 +0200

    Temperature conversion

---
+++ TemperatureConversion/Scripts/script.js
@@ -0,0 +1,37 @@
+﻿"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    var textField = document.getElementById("input_text");
+    var resultText = document.getElementById("output_kelvin");
+    var errorMessage = document.getElementById("error_massage");
+
+    document.getElementById("content_button").addEventListener("click", function () {
+        var text = textField.value;
+
+        if (text === "") {
+            errorMessage.textContent = "Please enter text";
+            return;
+        }
+
+        errorMessage.textContent = "";
+
+        function convertToKelvin(value) {
+            return value + 273.15;
+        }
+
+        text = parseFloat(text);
+
+
+        resultText.textContent = convertToKelvin(text);
+
+        //var message = document.createElement("label");
+        //message.textContent = text;
+
+
+        //resultText.appendChild(message);
+
+        //textField.value = "";
+
+    });
+});
