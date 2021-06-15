e52d4f55b8d1232102d50b405da4fde50d33a3a1
Sergei Malykh <sergej.malykh@gmail.com>
2021-06-15 23:45:33 +0200

    Phonebook_jQuery

---
+++ Phonebook_jQuery/Scripts/script.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+$(document).ready(function () {
+    var firstName = $("#firstname_input_text");
+    var lastName = $("#lastname_input_text");
+    var phoneNumber = $("#phone_number_input_text");
+
+
+    $("#add_button").click(function () {
+        var firstNameValue = firstName.val();
+        var lastNameValue = lastName.val();
+        var phoneNumberValue = phoneNumber.val();
+
+        if (firstNameValue === "") {
+            $("#firstname_input_text").after("<span class= \"error\">This field is empty</span>");
+            return;
+        }
+    });
+});
