42a3987f19b79047dd18737062640d540062da1b
Sergei Malykh <sergej.malykh@gmail.com>
2021-06-28 22:37:25 +0200

    Underscore_task

--- UnderscoreTask/Scripts/script.js
+++ UnderscoreTask/Scripts/script.js
@@ -1,81 +1,84 @@
 ﻿(function () {
     "use strict";
 
-    var actors =
-        [
-            {
-                name: "Paul",
-                lastname: "Walker",
-                age: 28
-            },
-            {
-                name: "Vin",
-                lastname: "Diesel",
-                age: 40
-            },
-            {
-                name: "Dwayne",
-                lastname: "Johnson",
-                age: 45
-            },
-            {
-                name: "Michelle",
-                lastname: "Rodriguez",
-                age: 43
-            },
-            {
-                name: "Jordana",
-                lastname: "Brewster",
-                age: 41
-            },
-            {
-                name: "Tyrese",
-                lastname: "Gibson",
-                age: 41
-            },
-            {
-                name: "Lucas",
-                lastname: "Black",
-                age: 38
-            },
-            {
-                name: "Gal",
-                lastname: "Gadot",
-                age: 37
-            },
-            {
-                name: "Kurt",
-                lastname: "Russell",
-                age: 70
-            },
-            {
-                name: "Charlize",
-                lastname: "Theron",
-                age: 50
-            }
-        ];
+    var actors = [
+        {
+            name: "Paul",
+            lastname: "Walker",
+            age: 28
+        },
+        {
+            name: "Vin",
+            lastname: "Diesel",
+            age: 40
+        },
+        {
+            name: "Dwayne",
+            lastname: "Johnson",
+            age: 45
+        },
+        {
+            name: "Michelle",
+            lastname: "Rodriguez",
+            age: 43
+        },
+        {
+            name: "Jordana",
+            lastname: "Brewster",
+            age: 41
+        },
+        {
+            name: "Tyrese",
+            lastname: "Gibson",
+            age: 41
+        },
+        {
+            name: "Lucas",
+            lastname: "Black",
+            age: 38
+        },
+        {
+            name: "Gal",
+            lastname: "Gadot",
+            age: 37
+        },
+        {
+            name: "Kurt",
+            lastname: "Russell",
+            age: 70
+        },
+        {
+            name: "Charlize",
+            lastname: "Theron",
+            age: 50
+        }
+    ];
 
     function getAverageAge(actors) {
         var sum = _.chain(actors)
             .pluck("age")
-            .reduce(function (sum, currentAge) { return sum + currentAge })
+            .reduce(function (sum, currentAge) {
+                return sum + currentAge;
+            }, 0)
             .value();
 
         return sum / _.size(actors);
     }
 
     function getPeopleAgeFrom20To30(actors) {
-        return _.chain(actors)
-            .filter(function (person) { return person.age >= 20 && person.age <= 30; })
-            .sortBy(actors, "age")
-            .reverse()
+        var sortedList = _.chain(actors)
+            .filter(function (person) {
+                return person.age >= 20 && person.age <= 30;
+            })
             .value();
+
+        return _.sortBy(sortedList, "age");
     }
 
     function addFullName(actors) {
         return _.each(actors, function (item) {
             item.fullName = item.name + " " + item.lastname;
-        })
+        });
     }
 
     console.log("Средний возраст людей из списка равен: ");
