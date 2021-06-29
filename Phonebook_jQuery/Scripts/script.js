ff7d7a2230e43f225f75bac6c9c29abea98de302
Sergei Malykh <sergej.malykh@gmail.com>
2021-06-29 21:46:32 +0200

    Phonebook_jQuery

--- Phonebook_jQuery/Scripts/script.js
+++ Phonebook_jQuery/Scripts/script.js
@@ -1,115 +1,79 @@
 ﻿"use strict";
 
 $(document).ready(function () {
-    var firstName = $("#first_name_input_text");
-    var lastName = $("#last_name_input_text");
-    var phoneNumber = $("#phone_number_input_text");
+    var firstNameField = $("#first_name_input_text");
+    var lastNameField = $("#last_name_input_text");
+    var phoneNumberField = $("#phone_number_input_text");
 
     var errorMessage = $("#error_message");
 
     var table = $("#table");
 
-    $(".form").each(function () {
-        var form = $(this);
+    $("#add_button").click(function () {
+        var lastNameValue = lastNameField.val().trim();
+        var firstNameValue = firstNameField.val().trim();
+        var phoneNumberValue = phoneNumberField.val().trim();
+        var phoneNumberIntValue = parseInt(phoneNumberValue.toString(), 10);
 
-        form.find(".input_text").addClass("empty_field");
+        if (lastNameValue === "" || firstNameValue === "" || phoneNumberValue === "") {
+            errorMessage.text("Все поля должны быть заполнены");
 
-        function checkInputFields() {
-            form.find(".input_text").each(function () {
-                if ($(this).val().trim() !== "") {
-                    $(this).removeClass("empty_field");
-                } else {
-                    $(this).addClass("empty_field");
-                }
+            $("#form").find(".input_text").each(function () {
+                $(this).toggleClass("empty_field", $(this).val().trim() === "");
             });
+
+            return;
         }
 
-        function selectEmptyFields(inputField) {
-            form.find(inputField).css({ "border-color": "#d8512d" });
+        $("#form").find(".input_text").removeClass("empty_field");
 
-            setTimeout(function () {
-                form.find(inputField).removeAttr("style");
-            }, 500);
+        if (isNaN(phoneNumberIntValue)) {
+            errorMessage.text("Поле должно содержать только цифры");
+            phoneNumberField.addClass("empty_field");
+            return;
         }
 
-        setInterval(function () {
-            checkInputFields();
+        var listItem = $("<tr>");
 
-            var emptyFieldsCount = form.find(".empty_field").length;
+        setViewMode();
 
-            if (emptyFieldsCount > 0) {
-                if ($("#add_button").hasClass("disabled")) {
-                    return false;
-                } else {
-                    $("#add_button").addClass("disabled");
-                }
-            } else {
-                $("#add_button").removeClass("disabled");
-            }
-        }, 500);
+        table.append(listItem);
 
-        $("#add_button").click(function () {
-            var lastNameValue = lastName.val().trim();
-            var firstNameValue = firstName.val().trim();
-            var phoneNumberValue = phoneNumber.val().trim();
-            var phoneNumberIntValue = parseInt(phoneNumberValue.toString(), 10);
+        updateTableId();
 
-            if ($(this).hasClass("disabled")) {
-                selectEmptyFields(".empty_field");
-                errorMessage.text("Все поля должны быть заполнены");
-                return false;
-            }
+        clearTextFields();
 
-            if (isNaN(phoneNumberIntValue)) {
-                selectEmptyFields(phoneNumber);
-                errorMessage.text("Поле должно содержать только цифры");
-                return false;
-            }
-
-            var listItem = $("<tr>");
-
+        function setViewMode() {
             listItem.html("<td class=\"id\"></td>" +
                 "<td class=\"last_name\"></td>" +
                 "<td class=\"first_name\"></td>" +
                 "<td class=\"phone_number\"></td>" +
                 "<td class=\"delete_button\"></td>");
 
-            function setViewMode() {
-                listItem.find(".id").text(updateTableId());
-                listItem.find(".last_name").text(lastNameValue);
-                listItem.find(".first_name").text(firstNameValue);
-                listItem.find(".phone_number").text(phoneNumberValue);
-                listItem.find(".delete_button").html("<button class=\"delete_button\" type=\"button\">Delete</button");
+            listItem.find(".last_name").text(lastNameValue);
+            listItem.find(".first_name").text(firstNameValue);
+            listItem.find(".phone_number").text(phoneNumberValue);
+            listItem.find(".delete_button").html("<button class=\"delete_button\" type=\"button\">Delete</button");
 
-                listItem.find(".delete_button").click(function () {
-                    listItem.remove();
-                    $(this).closest("td").remove();
-                    updateTableId();
-                });
-            }
-
-            setViewMode();
-
-            table.append(listItem);
-
-            updateTableId();
-
-            clearTextFields();
-        });
-
-        function updateTableId() {
-            $("#table tbody tr").each(function (i) {
-                var count = i + 1;
-
-                $(this).find(".id").text(count++);
+            listItem.find(".delete_button").click(function () {
+                listItem.remove();
+                updateTableId();
             });
         }
-
-        function clearTextFields() {
-            firstName.val("");
-            lastName.val("");
-            phoneNumber.val("");
-            $(".error").text("");
-        }
     });
+
+    function updateTableId() {
+        table.find("tbody tr").each(function (i) {
+            var count = i + 1;
+
+            $(this).find(".id").text(count);
+        });
+    }
+
+    function clearTextFields() {
+        firstNameField.val("");
+        lastNameField.val("");
+        phoneNumberField.val("");
+        errorMessage.text("");
+    }
 });
